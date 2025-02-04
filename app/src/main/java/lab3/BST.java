cd760121beeeb54828ad5b5739a98928d129829f
verkleh <verkleh@wwu.edu>
2025-02-03 21:24:08 -0800

    Implemented the isLeaf method.

--- app/src/main/java/lab3/BST.java
+++ app/src/main/java/lab3/BST.java
@@ -12,7 +12,13 @@ public class BST {
     /* return true iff Node n is a leaf node. a null node is not considered
      * a leaf. */
     public boolean isLeaf(Node n) {
-        return false; //TODO
+        if (n == null) {
+            return false;
+        }
+        if (n.left == null && n.right == null) {
+            return false;
+        }
+        return true; //TODO
     }
 
     /** return the number of nodes in the tree */
